d7101861a5f867bfe2789f52c03cf7ed42310d81
EA - Luís Gustavo Silva Melo <luis_melo@educadventista.org>
2023-10-10 21:58:53 -0300

    add repetition structure

---
+++ 3_HANDLEBARS/5_repeticao/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+    const items = ['item a', 'item b', 'item c']
+
+    res.render('dashboard', { items })
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        nome: "joão",
+        profissao: "Desenvolvedor",
+        idade: 24
+    }
+
+    const auth = true
+
+    res.render('home', { user, auth } )
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
